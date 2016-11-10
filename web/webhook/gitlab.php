9a20c80ce6c47542df6db7b8a4370ee20b555e58
rez1dent3 <maksim.babichev95@gmail.com>
2016-11-10 10:06:20 +0300

    patch gitlab

--- web/webhook/gitlab.php
+++ web/webhook/gitlab.php
@@ -1,20 +1,21 @@
 <?php
 
-$pswd = 'ltiN\p[R7Yz*nj/e';
+$pswd         = 'ltiN\p[R7Yz*nj/e';
 $access_token = '_D1d^+{NK#T.b9q-4*&IMHj:mJk"]Y[fCRA6l;89S0Us&cVQgWP?}!/E5wv7oXuZ';
 
-$client_token = $_SERVER['X-Gitlab-Token'];
-$client_ip = $_SERVER['REMOTE_ADDR'];
+$client_token = $_SERVER['X-GITLAB-TOKEN'];
+$client_ip    = $_SERVER['REMOTE_ADDR'];
 
 $fs = fopen('webhook.log', 'a');
-fwrite($fs, 'Request on ['.date("Y-m-d H:i:s").'] from ['.$client_ip.']'.PHP_EOL);
+fwrite($fs, 'Request on [' . date("Y-m-d H:i:s") . '] from [' . $client_ip . ']' . PHP_EOL);
 
 if ($client_token !== $access_token)
 {
     echo "error 403";
-    fwrite($fs, "Invalid token [{$client_token}]".PHP_EOL);
-    fwrite($fs, json_encode(['GET' => $_GET]).PHP_EOL);
-    fwrite($fs, json_encode(['POST' => $_POST]).PHP_EOL);
+    fwrite($fs, "Invalid token [{$client_token}]" . PHP_EOL);
+    fwrite($fs, json_encode(['GET' => $_GET]) . PHP_EOL);
+    fwrite($fs, json_encode(['POST' => $_POST]) . PHP_EOL);
+    fwrite($fs, json_encode(['SERVER' => $_SERVER]) . PHP_EOL);
     exit(0);
 }
 
@@ -22,21 +23,21 @@ $json = file_get_contents('php://input');
 $data = json_decode($json, true);
 
 $branch = $data["ref"];
-fwrite($fs, '======================================================================='.PHP_EOL);
+fwrite($fs, '=======================================================================' . PHP_EOL);
 
 if ($branch === 'refs/heads/master')
 {
-	fwrite($fs, 'BRANCH: '.print_r($branch, true).PHP_EOL);
-	fwrite($fs, '======================================================================='.PHP_EOL);
-	$fs and fclose($fs);
+    fwrite($fs, 'BRANCH: ' . print_r($branch, true) . PHP_EOL);
+    fwrite($fs, '=======================================================================' . PHP_EOL);
+    $fs and fclose($fs);
 
-	exec("/home/wheelpro/scripts/webhook/master.sh");
-} 
-else 
+    exec("/home/wheelpro/scripts/webhook/master.sh");
+}
+else
 {
-	fwrite($fs, 'BRANCH: '.print_r($branch, true).PHP_EOL);
-	fwrite($fs, '======================================================================='.PHP_EOL);
-	$fs and fclose($fs);
-	
-	exec("/home/wheelpro/scripts/webhook/dev.sh");
+    fwrite($fs, 'BRANCH: ' . print_r($branch, true) . PHP_EOL);
+    fwrite($fs, '=======================================================================' . PHP_EOL);
+    $fs and fclose($fs);
+
+    exec("/home/wheelpro/scripts/webhook/dev.sh");
 }
