fed4f8fc401ffb1ebe03b7c77eb1c522c2eb183c
rez1dent3 <maksim.babichev95@gmail.com>
2016-11-10 10:26:28 +0300

    temp

--- web/index.php
+++ web/index.php
@@ -1,7 +1,7 @@
 <?php
 
 require_once(__DIR__ . '/../vendor/autoload.php');
-
+var_dump(1);
 $framework = new Project\Framework();
 $framework->registerDebugHandlers();
 $framework->processHttpSapiRequest();
