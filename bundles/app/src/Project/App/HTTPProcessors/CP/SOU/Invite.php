464adb49fd67604847e9364636d8baf6c8b2e2e7
Babichev Maxim <maksim.babichev95@gmail.com>
2016-11-13 21:53:24 +0300

    add chosen modify add.haml add cp.js

--- bundles/app/src/Project/App/HTTPProcessors/CP/SOU/Invite.php
+++ bundles/app/src/Project/App/HTTPProcessors/CP/SOU/Invite.php
@@ -5,6 +5,7 @@ namespace Project\App\HTTPProcessors\CP\SOU;
 use PHPixie\HTTP\Request;
 use Project\App\HTTPProcessors\Processor\SOUProtected;
 use Project\App\Model;
+use Project\App\Role;
 
 class Invite extends SOUProtected
 {
@@ -19,7 +20,11 @@ class Invite extends SOUProtected
 
     public function addAction(Request $request)
     {
-        $this->variables['title'] = 'New Item';
+        $data = $request->data();
+
+        $inviteSystem = false;
+        $email        = $data->get('email');
+        $roleId       = $data->get('roleId', Role::User);
 
         if ($request->method() === 'POST')
         {
@@ -31,7 +36,7 @@ class Invite extends SOUProtected
 
             $data = $request->data();
 
-            $invite->email = $data->get('email');
+            $invite->email  = $data->get('email');
             $invite->roleId = $data->get('roleId');
 
             $invite->userId = $userId;
@@ -43,7 +48,21 @@ class Invite extends SOUProtected
             $chars = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ+-';
 
             $invite->token = $generator->generateString(64, $chars);
+
+            return $data->get();
         }
+
+        $roles = $this->components->orm()->query(Model::ROLE)
+            ->find()
+            ->asArray(true);
+
+        $this->variables['email']  = $email;
+        $this->variables['roleId'] = $roleId;
+
+        $this->variables['inviteSystem'] = $inviteSystem;
+        $this->variables['roles']        = $roles;
+
+        $this->variables['title'] = 'New Item';
 
         return $this->render('app:cp/sou/invite/add');
     }
