80e0daabf28537215500d2b35704b1c1cf150bf3
Babichev Maxim <maksim.babichev95@gmail.com>
2016-11-23 07:06:17 +0300

    try added oauth server in phpixie

--- bundles/api/src/Project/Api/HTTPProcessors/Auth.php
+++ bundles/api/src/Project/Api/HTTPProcessors/Auth.php
@@ -5,7 +5,25 @@ namespace Project\Api\HTTPProcessors;
 class Auth extends Processor
 {
 
-    protected $isProtected = false;
+    protected function pdo()
+    {
+        return $this->components->database()->get()->pdo();
+    }
+
+    public function authorizeAction()
+    {
+//        \OAuth2\GrantType\
+    }
+
+    public function tokenAction()
+    {
+
+    }
+
+    public function resourceAction()
+    {
+
+    }
 
     public function defaultAction()
     {
--- bundles/api/src/Project/Api/HTTPProcessors/Processor.php
+++ bundles/api/src/Project/Api/HTTPProcessors/Processor.php
@@ -13,39 +13,4 @@ abstract class Processor extends \Project\Extension\Processor
      */
     protected $resolverPath = 'api.processor';
 
-    /**
-     * @var bool
-     */
-    protected $isProtected = true;
-
-    public function token()
-    {
-        $user = $this->loggedUser();
-        var_dump($user->asObject(true));
-        die;
-    }
-
-    /**
-     * @param $value
-     *
-     * @return mixed
-     * @throws \PHPixie\Processors\Exception
-     */
-    public function process($value)
-    {
-        if (!$this->isProtected)
-        {
-            $token = $this->token(); // get Token
-        }
-
-        $method = $this->getMethodNameFor($value);
-
-        if ($method === null)
-        {
-            throw new \PHPixie\Processors\Exception("No action method found for value");
-        }
-
-        return $this->$method($value);
-    }
-
 }
