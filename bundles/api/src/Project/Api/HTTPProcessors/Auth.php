84faeb644e1f1097eb16420a42e21fada6d1ae20
rez1dent3 <maksim.babichev95@gmail.com>
2016-11-23 16:28:54 +0300

    add oauth server

--- bundles/api/src/Project/Api/HTTPProcessors/Auth.php
+++ bundles/api/src/Project/Api/HTTPProcessors/Auth.php
@@ -2,32 +2,90 @@
 
 namespace Project\Api\HTTPProcessors;
 
+use OAuth2\GrantType\AuthorizationCode;
+use OAuth2\GrantType\ClientCredentials;
+use OAuth2\GrantType\RefreshToken;
+use OAuth2\GrantType\UserCredentials;
+use PHPixie\HTTP\Request;
+
 class Auth extends Processor
 {
 
-    protected function pdo()
+    protected function globalsRequest()
     {
-        return $this->components->database()->get()->pdo();
+        return \OAuth2\Request::createFromGlobals();
+    }
+
+    protected function server()
+    {
+        $storage = new \Project\OAuth2\PDO($this->builder);
+
+        $server = new \OAuth2\Server($storage, [
+            'access_lifetime' => 86400 * 365.25 * 3
+        ]);
+
+        $server->addGrantType(new ClientCredentials($storage));
+        $server->addGrantType(new AuthorizationCode($storage));
+        $server->addGrantType(new RefreshToken($storage));
+        $server->addGrantType(new UserCredentials($storage));
+
+        return $server;
+    }
+
+    /**
+     * @param Request $request
+     *
+     * @return array|null|string
+     */
+    public function callbackAction(Request $request)
+    {
+        return $request->data()->get();
     }
 
     public function authorizeAction()
     {
-//        \OAuth2\GrantType\
+        $this->server();
     }
 
+    /**
+     * http -a testC:testS -f POST wbs-cms/api/auth/token grant_type=password username=$USER$ password=$PASSWORD$
+     *
+     * @return mixed|string
+     * @throws \OAuth2\InvalidArgumentException
+     * @throws \OAuth2\LogicException
+     */
     public function tokenAction()
     {
-
+        return $this->server()
+            ->handleTokenRequest($this->globalsRequest())
+            ->getResponseBody();
     }
 
+    /**
+     * http -f POST wbs-cms/api/auth/resource access_token=$TOKEN$
+     *
+     * @return array|mixed|string
+     */
     public function resourceAction()
     {
+        $server         = $this->server();
+        $globalsRequest = $this->globalsRequest();
 
-    }
+        if (!$server->verifyResourceRequest($globalsRequest))
+        {
+            /**
+             * @var $response \OAuth2\Response
+             */
+            $response = $server->getResponse();
 
-    public function defaultAction()
-    {
-        return [];
+            return $response->getResponseBody();
+        }
+
+        return array(
+            'access_token' => $server->getAccessTokenData($globalsRequest),
+            'success'      => true,
+            'message'      => 'You accessed my APIs!'
+        );
     }
 
 }
---
+++ src/Project/OAuth2/PDO.php
@@ -0,0 +1,140 @@
+<?php
+
+namespace Project\OAuth2;
+
+use Project\Api;
+use PHPixie\AuthLogin\Providers\Password as PasswordProvider;
+use Project\Model;
+use Project\ORM\User\Query;
+
+class PDO extends \OAuth2\Storage\Pdo
+{
+
+    /**
+     * @var Api\Builder $builder
+     */
+    protected $builder;
+
+    /**
+     * PDO constructor.
+     *
+     * @param Api\Builder $builder
+     */
+    public function __construct($builder)
+    {
+        $this->builder = $builder;
+
+        $pdo    = $builder->components()->database()->get()->pdo();
+        $config = $builder->bundleConfig()->get('oauth', []);
+
+        parent::__construct($pdo, $config);
+    }
+
+    /**
+     * @param string $login
+     * @param string $password
+     *
+     * @return bool
+     */
+    protected function checkPassword($login, $password)
+    {
+        $domain = $this->builder->components()->auth()->domain();
+
+        /**
+         * @var PasswordProvider $passwordProvider
+         */
+        $passwordProvider = $domain->provider('password');
+
+        $user = $passwordProvider->login($login, $password);
+
+        return $user !== null;
+    }
+
+    /**
+     * @param $login
+     * @param $password
+     *
+     * @return bool
+     */
+    public function checkUserCredentials($login, $password)
+    {
+        return $this->checkPassword($login, $password);
+    }
+
+    /**
+     * @param $login
+     *
+     * @return array|bool
+     */
+    public function getUser($login)
+    {
+        $orm = $this->builder->components()->orm();
+
+        /**
+         * @var $queryUser Query
+         */
+        $queryUser = $orm->query(Model::USER);
+
+        $user = $queryUser->findByLogin($login);
+
+        if (!$user)
+        {
+            return false;
+        }
+
+        $user->user_id = $user->id();
+
+        return (array)$user->asObject(true);
+    }
+
+    /**
+     * @param      $login
+     * @param      $password
+     * @param null $firstName
+     * @param null $lastName
+     *
+     * @return bool
+     */
+    public function setUser($login, $password, $firstName = null, $lastName = null)
+    {
+        $orm = $this->builder->components()->orm();
+
+        /**
+         * @var $queryUser Query
+         */
+        $queryUser = $orm->query(Model::USER);
+
+        $user = $queryUser->findByLogin($login);
+
+        $domain = $this->builder->components()->auth()->domain();
+
+        /**
+         * @var PasswordProvider $passwordProvider
+         */
+        $passwordProvider = $domain->provider('password');
+
+        $passwordHash = $passwordProvider->hash($password);
+
+        if (!$user)
+        {
+            $user = $orm->createEntity(Model::USER);
+        }
+
+        if ($firstName)
+        {
+            $user->name = $firstName;
+        }
+
+        if ($lastName)
+        {
+            $user->lastName = $lastName;
+        }
+
+        $user->passwordHash = $passwordHash;
+
+        $user->save();
+
+        return $user->id() > 0;
+    }
+
+}
