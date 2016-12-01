9d03f1c805dc766963ecf9aeaa515f5f7d8924b3
Babichev Maxim <maksim.babichev95@gmail.com>
2016-12-01 20:52:34 +0300

    patch

--- bundles/app/src/Project/App/HTTPProcessors/Webhook.php
+++ bundles/app/src/Project/App/HTTPProcessors/Webhook.php
@@ -84,6 +84,8 @@ class Webhook extends Processor
 
             $this->shellExec("git pull origin {$branch}");
 
+            $this->shellExec("ln -s /home/wheelpro/web/config/{$branch}.php /home/wheelpro/web/{$dirName}/assets/parameters.php");
+            
             $this->shellExec('chown -R wheelpro:www-group *');
 
             $this->shellExec('composer install');
