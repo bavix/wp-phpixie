81f7d24b77b0ca1e0ab3b9ccb5ada281bb4b105c
Babichev Maxim <maksim.babichev95@gmail.com>
2016-12-13 19:38:46 +0300

    patch brand list

--- web/js/cp/brand/list.js
+++ web/js/cp/brand/list.js
@@ -1,6 +1,16 @@
+"use strict";
+
 // ReactDOM.render(
 //     <h1>Hello, world!</h1>,
 //     document.getElementById('root')
 // );
-"use strict";
+
+var filter = {
+    data: function data(params) {
+        return {
+            term: params.term || "",
+            page: params.page || 1
+        };
+    }
+};
 //# sourceMappingURL=list.js.map
