0eb29b703d2e13524066baf03438870a23bcd1e2
rez1dent3 <maksim.babichev95@gmail.com>
2017-02-06 16:34:35 +0300

    update http

---
+++ web/js/cp/sow/bolt-pattern/add.js
@@ -0,0 +1,41 @@
+'use strict';
+
+$(function () {
+
+    $('[data-created]').submit(function (event) {
+
+        event.preventDefault();
+
+        var $self = $(this);
+        var form = new FormData(this);
+
+        fetch($self.attr('action'), {
+            method: $self.attr('method'),
+            credentials: 'include',
+            body: form
+        }).then(function (response) {
+            if (response.status === 201 || response.status === 200) {
+                return response.json();
+            }
+
+            var error = new Error(response.statusText);
+            error.response = response;
+            throw error;
+        }).then(function (json) {
+
+            location.href = '/cp/sow/bolt-pattern/edit/' + json.id;
+        }).catch(function (error) {
+            var $message = $self.find('.alert');
+
+            if (!$message.length) {
+                $self.find('div:first-child').prepend('<div class="alert"></div>');
+                $message = $self.find('.alert');
+            }
+
+            error.response.json().then(function (json) {
+                return $message.addClass('alert-danger').text(json.error_description);
+            });
+        });
+    });
+});
+//# sourceMappingURL=add.js.map
