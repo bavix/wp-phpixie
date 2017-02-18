cf52c34c19ac0e7e85e64d02d72e228a253d92dc
Babichev Maxim <info@babichev.net>
2017-02-18 15:04:48 +0300

    remove return

--- bundles/api/src/Project/Api/HTTPProcessors/Upload.php
+++ bundles/api/src/Project/Api/HTTPProcessors/Upload.php
@@ -52,8 +52,6 @@ class Upload extends AuthProcessor
 //        query.userId      // userId
 //        data.Filename     // file name
 
-        return $data->get();
-
         $logo              = $this->components->orm()->createEntity(Model::BRAND_LOGO);
         $logo->hash        = $data->get('hash');
         $logo->size        = $data->get('fileSize');
