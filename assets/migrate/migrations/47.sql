a856d706bdf5ee2f5bbd84c47ead7fcfc2f02f3a
Babichev Maxim <info@babichev.net>
2017-03-06 18:26:03 +0300

    remove named

--- assets/migrate/migrations/47.sql
+++ assets/migrate/migrations/47.sql
@@ -1,6 +1,6 @@
 ALTER TABLE `videos` ADD `identifier` VARCHAR(50) NULL AFTER `id`;
-ALTER TABLE `wheelsLikes` ADD UNIQUE `unique_index` ( `wheelId`, `userId`);
-ALTER TABLE `wheelsFavourites` ADD UNIQUE `unique_index` ( `wheelId`, `userId`);
-ALTER TABLE `wheelsImages` ADD UNIQUE `unique_index` ( `wheelId`, `imageId`);
-ALTER TABLE `wheelsComments` ADD UNIQUE `unique_index` ( `wheelId`, `commentId`);
-ALTER TABLE `wheelsVideos` ADD UNIQUE `unique_index` ( `wheelId`, `videoId`);
+ALTER TABLE `wheelsLikes` ADD UNIQUE ( `wheelId`, `userId`);
+ALTER TABLE `wheelsFavourites` ADD UNIQUE ( `wheelId`, `userId`);
+ALTER TABLE `wheelsImages` ADD UNIQUE ( `wheelId`, `imageId`);
+ALTER TABLE `wheelsComments` ADD UNIQUE ( `wheelId`, `commentId`);
+ALTER TABLE `wheelsVideos` ADD UNIQUE ( `wheelId`, `videoId`);
