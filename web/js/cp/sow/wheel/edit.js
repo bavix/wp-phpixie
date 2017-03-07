c7805d3ea0f83a3c8e85b1ef603bf2da68183197
REZ1DENT3 <maksim.babichev95@gmail.com>
2017-03-07 16:14:42 +0300

    add video

--- web/js/cp/sow/wheel/edit.js
+++ web/js/cp/sow/wheel/edit.js
@@ -1,4 +1,68 @@
-'use strict';
+"use strict";
+
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
+
+function response(response) {
+    if (response.status === 201 || response.status === 200) {
+        return response.json();
+    }
+
+    if (response.status !== 204) {
+        var error = new Error(response.statusText);
+        error.response = response;
+        throw error;
+    }
+
+    return [];
+}
+
+var VideoRows = function (_React$Component) {
+    _inherits(VideoRows, _React$Component);
+
+    function VideoRows(props) {
+        _classCallCheck(this, VideoRows);
+
+        var _this = _possibleConstructorReturn(this, (VideoRows.__proto__ || Object.getPrototypeOf(VideoRows)).call(this, props));
+
+        _this.state = {
+            data: props.rows
+        };
+        return _this;
+    }
+
+    _createClass(VideoRows, [{
+        key: "row",
+        value: function row(model) {
+            var provider = String(model.provider).toLowerCase();
+
+            return React.createElement(
+                "div",
+                { key: model.id, className: "col-md-6" },
+                React.createElement("div", { "data-type": provider, "data-video-id": model.identifier })
+            );
+        }
+    }, {
+        key: "render",
+        value: function render() {
+
+            var rows = this.props.rows.map(this.row);
+
+            return React.createElement(
+                "div",
+                { className: "col-xs-12" },
+                rows
+            );
+        }
+    }]);
+
+    return VideoRows;
+}(React.Component);
 
 $(function () {
 
@@ -42,6 +106,49 @@ $(function () {
     // fixme
     $('#styleWheel').select2({
         theme: "bootstrap"
+    });
+
+    // video
+
+    var socialRows = document.getElementById('videoRows');
+    var $formVideo = $('[data-created="video"]');
+
+    var videoJson = [];
+
+    function tableInit(json) {
+        if (typeof json.id === "undefined") {
+            videoJson = json.data;
+        } else {
+            videoJson.push(json);
+        }
+
+        ReactDOM.render(React.createElement(VideoRows, { rows: videoJson }), videoRows);
+
+        plyr.setup();
+    }
+
+    // init data
+    fetch($formVideo.attr('action') + '?terms[provider]=YouTube', {
+        method: 'GET',
+        credentials: 'include'
+    }).then(response).then(tableInit).catch(function () {
+        return undefined;
+    });
+
+    // send form
+    $formVideo.submit(function (event) {
+
+        event.preventDefault();
+
+        var form = new FormData(this);
+
+        fetch($formVideo.attr('action'), {
+            method: $formVideo.attr('method'),
+            credentials: 'include',
+            body: form
+        }).then(response).then(tableInit).catch(function () {
+            return undefined;
+        });
     });
 });
 //# sourceMappingURL=edit.js.map
--- web/js/cp/sow/wheel/edit.jsx
+++ web/js/cp/sow/wheel/edit.jsx
@@ -1,3 +1,45 @@
+function response(response) {
+    if (response.status === 201 || response.status === 200) {
+        return response.json();
+    }
+
+    if (response.status !== 204) {
+        let error = new Error(response.statusText);
+        error.response = response;
+        throw error;
+    }
+
+    return [];
+}
+
+class VideoRows extends React.Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            data: props.rows
+        };
+    }
+
+    row(model) {
+        let provider = String(model.provider).toLowerCase();
+
+        return <div key={model.id} className="col-md-6">
+            <div data-type={provider} data-video-id={model.identifier}></div>
+        </div>;
+    }
+
+    render() {
+
+        const rows = this.props.rows.map(this.row);
+
+        return <div className="col-xs-12">
+            { rows }
+        </div>;
+    }
+}
+
 $(function () {
 
     $('[data-updated="wheel"]').submit(function (event) {
@@ -40,6 +82,51 @@ $(function () {
     // fixme
     $('#styleWheel').select2({
         theme: "bootstrap"
+    });
+
+    // video
+
+    const socialRows = document.getElementById('videoRows');
+    let $formVideo = $('[data-created="video"]');
+
+    let videoJson = [];
+
+    function tableInit(json) {
+        if (typeof json.id === "undefined") {
+            videoJson = json.data;
+        }
+        else {
+            videoJson.push(json)
+        }
+
+        ReactDOM.render(
+            <VideoRows rows={videoJson}/>,
+            videoRows
+        );
+
+        plyr.setup();
+
+    }
+
+    // init data
+    fetch($formVideo.attr('action') + '?terms[provider]=YouTube', {
+        method: 'GET',
+        credentials: 'include'
+    }).then(response).then(tableInit).catch(() => undefined);
+
+    // send form
+    $formVideo.submit(function (event) {
+
+        event.preventDefault();
+
+        let form = new FormData(this);
+
+        fetch($formVideo.attr('action'), {
+            method: $formVideo.attr('method'),
+            credentials: 'include',
+            body: form
+        }).then(response).then(tableInit).catch(() => undefined);
+
     });
 
 });
