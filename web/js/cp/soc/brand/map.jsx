342d257e2ba3fb3a6814c999ee004a4bcca66166
Babichev Maxim <info@babichev.net>
2017-03-07 23:46:32 +0300

    update

--- web/js/cp/soc/brand/map.jsx
+++ web/js/cp/soc/brand/map.jsx
@@ -79,6 +79,8 @@ function toForm(place) {
         return;
     }
 
+    console.debug(place.address_components);
+
     // Get each component of the address from the place details
     // and fill the corresponding field on the form.
     for (let i = 0; i < place.address_components.length; i++) {
