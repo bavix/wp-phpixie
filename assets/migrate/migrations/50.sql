cd40f35064bdc3002663fe018fdbea4c5e7987e4
Babichev Maxim <info@babichev.net>
2017-03-13 21:58:51 +0300

    update

--- assets/migrate/migrations/50.sql
+++ assets/migrate/migrations/50.sql
@@ -1,18 +1,6 @@
--- Adminer 4.2.5 MySQL dump
-
-SET NAMES utf8;
-SET time_zone = '+00:00';
-SET foreign_key_checks = 0;
-SET sql_mode = 'NO_AUTO_VALUE_ON_ZERO';
-
-SET NAMES utf8mb4;
-
 CREATE TABLE `dealersAddresses` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `dealerId` int(11) NOT NULL,
   `addressId` int(11) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
-
--- 2017-03-13 18:56:24
